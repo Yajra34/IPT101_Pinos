a0e24ffef4295cd715cbdc8b12f97f5ed2c46abe
Yajra34 <123623808+Yajra34@users.noreply.github.com>
2023-03-28 16:22:31 +0800

    Add files via upload

---
+++ Multiplication Table - PINOS.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <body>
+            <?php
+            $number = 5;
+            $i = 1;
+            $y = 0;
+            echo "Multiplication Table of 5: <br>";
+            for($i = 1; $i <= 10; $i++){
+                $y = $number * $i;
+                echo "$number x $i = $y  <br>" ;
+            } 
+            ?>
+        </body>
+</hea>
+
+</html>
